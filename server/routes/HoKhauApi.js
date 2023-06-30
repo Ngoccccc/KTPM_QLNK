d6b9b52b5d092e5d75d80e9cd13efda29acde072
tnt2402 <nguyenthanh24022002@gmail.com>
2023-07-01 00:17:04 +0700

    Update HoKhauApi.js

--- server/routes/HoKhauApi.js
+++ server/routes/HoKhauApi.js
@@ -7,19 +7,58 @@ var Thuoc = require("../models/Thuoc");
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
 var conn = require("../models/connectDB");
-const { QueryTypes, where } = require("sequelize");
+const { QueryTypes, where, json } = require("sequelize");
 
 var router = express.Router();
 
 router.get("/", async function (req, res, next) {
   try {
-    var arr = [];
-    (await SoHoKhau.findAll()).forEach(function (item) {
-      arr.push(item);
-    });
-    res.json(arr);
-  } catch (error) {}
+    var arr = await SoHoKhau.findAll();
+    var result = [];
+
+    for (const item of arr) {
+      var result_chuHo = await ChuHo.findOne({
+        where: { soHoKhau: item.soHoKhau },
+      });
+      
+      if (result_chuHo) {
+        var result_thongTinChuHo = await NhanKhau.findOne({
+          where: { soCCCD: result_chuHo.soCCCD },
+        });
+
+        item.dataValues.chuho = result_thongTinChuHo;
+      }
+
+      result.push( item );
+    }
+
+    res.json(result);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: "Internal Error" });  }
 });
+// router.get("/", async function (req, res, next) {
+//   try {
+//     var arr = [];
+//     var result = []
+//     // (await SoHoKhau.findAll()).forEach(function (item) {
+//     //   arr.push(item);
+//     // });
+
+//     var arr = await SoHoKhau.findAll();
+
+//     arr.forEach(function(item) {
+//       // Perform an action with item.soHoKhau
+//       var result_chuHo = ChuHo.findAll({
+//         where: { soHoKhau: item.soHoKhau },
+//       });
+//       item.push({"chuho": result_chuHo})
+//       tmp = json({"thong tin ho khau": item})
+//       result.push(tmp)
+//     });
+//     res.json(result);
+//   } catch (error) {}
+// });
 router.post("/", async function (req, res, next) {
   try {
     var result = await SoHoKhau.findOne({
